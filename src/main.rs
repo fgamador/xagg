695cbd99493c00a58441a5b75550828bdbdab528
Franz Amador <franzamador@gmail.com>
2021-04-05 20:25:04 -0700

    demo csv reading

--- src/main.rs
+++ src/main.rs
@@ -1,3 +1,18 @@
+use std::error::Error;
+use std::process;
+
+fn example() -> Result<(), Box<dyn Error>> {
+    let mut rdr = csv::Reader::from_path("input/checking/transactions.csv")?;
+    for result in rdr.records() {
+        let record = result?;
+        println!("{:?}", record);
+    }
+    Ok(())
+}
+
 fn main() {
-    println!("Hello, world!");
+    if let Err(err) = example() {
+        println!("error running example: {}", err);
+        process::exit(1);
+    }
 }
