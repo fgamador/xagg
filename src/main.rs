da1ea52db1b8a8d6d679e9e1b8b92fef2b3e0fa8
Franz Amador <franzamador@gmail.com>
2021-04-05 21:27:30 -0700

    Add JSON reading example

--- src/main.rs
+++ src/main.rs
@@ -1,7 +1,17 @@
+//use serde::Deserialize;
+use serde::Deserialize;
 use std::error::Error;
+use std::fs;
 use std::process;
 
-fn example() -> Result<(), Box<dyn Error>> {
+#[derive(Debug, Deserialize)]
+struct CsvConfig {
+    date_index: u32,
+    description_index: u32,
+    amount_index: u32,
+}
+
+fn csv_example() -> Result<(), Box<dyn Error>> {
     let mut rdr = csv::Reader::from_path("input/checking/transactions.csv")?;
     for result in rdr.records() {
         let record = result?;
@@ -10,9 +20,21 @@ fn example() -> Result<(), Box<dyn Error>> {
     Ok(())
 }
 
+fn json_example() -> serde_json::Result<()> {
+    let contents = fs::read_to_string("input/checking/config.json")
+        .expect("Something went wrong reading the file");
+    let config: CsvConfig = serde_json::from_str(&contents)?;
+    println!("{:?}", config);
+    Ok(())
+}
+
 fn main() {
-    if let Err(err) = example() {
-        println!("error running example: {}", err);
+    if let Err(err) = csv_example() {
+        println!("error running csv_example: {}", err);
+        process::exit(1);
+    }
+    if let Err(err) = json_example() {
+        println!("error running json_example: {}", err);
         process::exit(1);
     }
 }
