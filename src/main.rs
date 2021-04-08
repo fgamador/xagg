0ee03565e6d131e054f708cc5d346b33fc8af87e
Franz Amador <franzamador@gmail.com>
2021-04-07 21:37:24 -0700

    fn to get csv-file paths

--- src/main.rs
+++ src/main.rs
@@ -1,8 +1,11 @@
 use serde::Deserialize;
 use std::error::Error;
 use std::fs;
+use std::io;
 use std::process;
 use std::fs::DirEntry;
+use csv::{StringRecord, StringRecordIter};
+use std::path::PathBuf;
 
 #[derive(Debug, Deserialize)]
 struct CsvConfig {
@@ -29,9 +32,9 @@ fn _json_example() -> serde_json::Result<()> {
     Ok(())
 }
 
-fn dir_example() -> Result<(), Box<dyn Error>> {
+fn dir_example() -> io::Result<()> {
     let it = fs::read_dir("input")?
-        .map(|result| result.map(dir_entry_to_pair));
+        .map(|result| result.map(input_subdir_to_pair));
 
     for item in it {
         println!("{:?}", item?);
@@ -39,12 +42,12 @@ fn dir_example() -> Result<(), Box<dyn Error>> {
     Ok(())
 }
 
-fn dir_entry_to_pair(entry: DirEntry) -> CsvConfig {
-    dir_entry_to_config(&entry)
+fn input_subdir_to_pair(subdir: DirEntry) -> CsvConfig {
+    input_subdir_to_csv_config(&subdir)
 }
 
-fn dir_entry_to_config(entry: &DirEntry) -> CsvConfig {
-    let mut config_path = entry.path();
+fn input_subdir_to_csv_config(subdir: &DirEntry) -> CsvConfig {
+    let mut config_path = subdir.path();
     config_path.push("config.json");
     let contents = fs::read_to_string(config_path)
         .expect("fs::read_to_string error");
@@ -52,6 +55,26 @@ fn dir_entry_to_config(entry: &DirEntry) -> CsvConfig {
         .expect("serde_json::from_str error");
     config
 }
+
+fn input_subdir_to_csv_file_path_iter(subdir: &DirEntry) -> impl Iterator<Item=PathBuf> {
+    fs::read_dir(subdir.path()).unwrap()
+        .filter_map(|result| {
+            let entry = result.unwrap();
+            if entry.file_name().into_string().unwrap().to_lowercase().ends_with(".csv") {
+                Some(entry.path())
+            } else {
+                None
+            }
+        })
+}
+
+// fn input_dir_entry_to_csv_line_iter(entry: &DirEntry) -> std::slice::Iter<'_, StringRecord> {
+//     let mut rdr = csv::Reader::from_path("input/checking/transactions.csv")?;
+//     for result in rdr.records() {
+//         let record = result?;
+//         println!("{:?}", record);
+//     }
+// }
 
 fn main() {
     if let Err(err) = dir_example() {
