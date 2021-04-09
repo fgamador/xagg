5aca1dc3aca3e5faa2b266611818ce6dc6243590
Franz Amador <franzamador@gmail.com>
2021-04-08 21:37:34 -0700

    Tidy up API

--- src/main.rs
+++ src/main.rs
@@ -1,10 +1,8 @@
 use csv::StringRecord;
 use serde::Deserialize;
 use std::fs;
-use std::io;
-use std::process;
 use std::fs::DirEntry;
-use std::path::PathBuf;
+use std::path::{Path, PathBuf};
 
 #[derive(Debug, Deserialize)]
 struct CsvConfig {
@@ -14,23 +12,30 @@ struct CsvConfig {
     amount_index: u32,
 }
 
-fn dir_example() -> io::Result<()> {
-    let it = fs::read_dir("input")?
-        .map(|result| result.map(input_subdir_to_pair));
-
-    for item in it {
-        let pair = item?;
+fn input_iter_example() {
+    for pair in input_dir_to_pair_iterator(&Path::new("input")) {
         println!("{:?}", pair.0);
         for path in pair.1 {
             println!("{:?}", path);
         }
     }
-    Ok(())
 }
 
-fn input_subdir_to_pair(subdir: DirEntry) -> (CsvConfig, impl Iterator<Item=csv::Result<StringRecord>>) {
-    (input_subdir_to_csv_config(&subdir),
-     csv_file_path_iter_to_csv_record_iter(input_subdir_to_csv_file_path_iter(&subdir)))
+fn input_dir_to_pair_iterator(
+    dir: &Path,
+) -> impl Iterator<Item = (CsvConfig, impl Iterator<Item = csv::Result<StringRecord>>)> {
+    fs::read_dir(dir)
+        .unwrap()
+        .map(|result| input_subdir_to_pair(result.unwrap()))
+}
+
+fn input_subdir_to_pair(
+    subdir: DirEntry,
+) -> (CsvConfig, impl Iterator<Item = csv::Result<StringRecord>>) {
+    (
+        input_subdir_to_csv_config(&subdir),
+        csv_file_path_iter_to_csv_record_iter(input_subdir_to_csv_file_path_iter(&subdir)),
+    )
 }
 
 fn input_subdir_to_csv_config(subdir: &DirEntry) -> CsvConfig {
@@ -40,27 +45,34 @@ fn input_subdir_to_csv_config(subdir: &DirEntry) -> CsvConfig {
     serde_json::from_str(&contents).unwrap()
 }
 
-fn input_subdir_to_csv_file_path_iter(subdir: &DirEntry) -> impl Iterator<Item=PathBuf> {
-    fs::read_dir(subdir.path()).unwrap()
-        .filter_map(|result| {
-            let entry = result.unwrap();
-            if entry.file_name().into_string().unwrap().to_lowercase().ends_with(".csv") {
-                Some(entry.path())
-            } else {
-                None
-            }
-        })
+fn input_subdir_to_csv_file_path_iter(subdir: &DirEntry) -> impl Iterator<Item = PathBuf> {
+    fs::read_dir(subdir.path()).unwrap().filter_map(|result| {
+        let entry = result.unwrap();
+        if entry
+            .file_name()
+            .into_string()
+            .unwrap()
+            .to_lowercase()
+            .ends_with(".csv")
+        {
+            Some(entry.path())
+        } else {
+            None
+        }
+    })
 }
 
-fn csv_file_path_iter_to_csv_record_iter<I>(paths: I) -> impl Iterator<Item=csv::Result<StringRecord>>
-    where I: Iterator<Item=PathBuf>
+fn csv_file_path_iter_to_csv_record_iter<I>(
+    paths: I,
+) -> impl Iterator<Item = csv::Result<StringRecord>>
+where
+    I: Iterator<Item = PathBuf>,
 {
-    paths.map(|path| csv::Reader::from_path(path).unwrap().into_records()).flatten()
+    paths
+        .map(|path| csv::Reader::from_path(path).unwrap().into_records())
+        .flatten()
 }
 
 fn main() {
-    if let Err(err) = dir_example() {
-        println!("error running dir_example: {}", err);
-        process::exit(1);
-    }
+    input_iter_example();
 }
