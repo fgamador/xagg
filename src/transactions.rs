35ccebe2a1b3c6b894fcbc9f1afb98fae6570686
Franz Amador <franzamador@gmail.com>
2021-04-10 11:01:05 -0700

    t create_transaction_from_csv_record (first field)

--- src/transactions.rs
+++ src/transactions.rs
@@ -3,10 +3,23 @@ use serde::Deserialize;
 
 #[derive(Debug, Deserialize)]
 pub struct CsvConfig {
-    date_index: u32,
+    date_index: usize,
     date_format: String,
-    description_index: u32,
-    amount_index: u32,
+    description_index: usize,
+    amount_index: usize,
+}
+
+pub struct Transaction {
+    raw_description: String,
+}
+
+pub fn csv_record_to_transaction(csv_record: &StringRecord, csv_config: &CsvConfig) -> Transaction {
+    Transaction {
+        raw_description: csv_record
+            .get(csv_config.description_index)
+            .unwrap_or("")
+            .to_string(),
+    }
 }
 
 #[cfg(test)]
@@ -15,15 +28,22 @@ mod tests {
 
     #[test]
     fn create_transaction_from_csv_record() {
-        let _csv_config = CsvConfig {
+        let csv_config = CsvConfig {
             date_index: 1,
             date_format: "%m/%d/%Y".to_string(),
             description_index: 2,
             amount_index: 4,
         };
-        let csv_record =
-            StringRecord::from(vec!["foo", "2/12/2020", "A transaction", "bar", "-12.93"]);
+        let csv_record = StringRecord::from(vec![
+            "noise",
+            "2/12/2020",
+            "ACME FALAFEL",
+            "noise",
+            "-12.93",
+        ]);
 
-        assert_eq!(csv_record.get(3), Some("bar"));
+        let transaction = csv_record_to_transaction(&csv_record, &csv_config);
+
+        assert_eq!(transaction.raw_description, "ACME FALAFEL");
     }
 }
