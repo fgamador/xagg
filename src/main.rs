7e186457dff3568517da68d6e63d55720cf94de4
Franz Amador <franzamador@gmail.com>
2022-06-26 11:59:57 -0700

    Reorder fns

--- src/main.rs
+++ src/main.rs
@@ -8,6 +8,125 @@ use serde::Serialize;
 use std::collections::{BTreeMap, HashMap, HashSet};
 use std::path::PathBuf;
 
+#[derive(Debug, Serialize)]
+pub struct TransactionDataNode {
+    name: String,
+    value: u32,
+}
+
+#[derive(Debug, Serialize)]
+pub struct DescriptionDataNode {
+    name: String,
+    children: Vec<TransactionDataNode>,
+}
+
+#[derive(Debug, Serialize)]
+pub struct CategoryDataNode {
+    name: String,
+    children: Vec<DescriptionDataNode>,
+}
+
+#[derive(Debug, Serialize)]
+pub struct RootDataNode {
+    name: String,
+    children: Vec<CategoryDataNode>,
+}
+
+type TransactionDataNodes = Vec<TransactionDataNode>;
+type DescriptionMap = HashMap<String, TransactionDataNodes>;
+type CategoryMap = HashMap<String, DescriptionMap>;
+
+fn main() {
+    generate_icicle_chart_data();
+}
+
+fn generate_icicle_chart_data() {
+    let category_map = gather_category_map();
+    let category_data_nodes = categories_to_data_nodes(category_map);
+    let root_data_node = RootDataNode {
+        name: "Spending".to_string(),
+        children: category_data_nodes,
+    };
+    println!("{}", serde_json::to_string(&root_data_node).unwrap());
+}
+
+fn gather_category_map() -> CategoryMap {
+    let rules = read_rules(PathBuf::from("input"));
+    let classifier = TransactionClassifier::new(rules);
+
+    let mut categories: CategoryMap = HashMap::new();
+
+    for (source, csv_config, cvs_records) in read_input(PathBuf::from("input")) {
+        for csv_record in cvs_records {
+            let transaction = classifier
+                .classify_transaction(csv_record_to_transaction(&csv_record, &csv_config));
+            if !should_exclude_transaction(&transaction) {
+                let descriptions = categories
+                    .entry(transaction.category.clone())
+                    .or_insert(HashMap::new());
+                let transactions = descriptions
+                    .entry(transaction.description.clone())
+                    .or_insert(vec![]);
+                if transaction.amount <= 0.0 {
+                    transactions.push(transaction_to_data_node(&source, transaction));
+                }
+            }
+        }
+    }
+
+    categories
+}
+
+fn transaction_to_data_node(source: &str, transaction: Transaction) -> TransactionDataNode {
+    TransactionDataNode {
+        name: format!("{}, {},", transaction.date, source),
+        value: -transaction.amount.round() as u32,
+    }
+}
+
+fn categories_to_data_nodes(category_map: CategoryMap) -> Vec<CategoryDataNode> {
+    let mut category_data_nodes = vec![];
+    for (category, description_map) in category_map {
+        let mut category_data_node = CategoryDataNode {
+            name: category,
+            children: vec![],
+        };
+        for (description, transaction_data_nodes) in description_map {
+            category_data_node.children.push(DescriptionDataNode {
+                name: description,
+                children: transaction_data_nodes,
+            });
+        }
+        category_data_nodes.push(category_data_node);
+    }
+    category_data_nodes
+}
+
+fn should_exclude_transaction(transaction: &Transaction) -> bool {
+    let exclude_categories: HashSet<&'static str> =
+        ["Dividend", "Salary", "Stock", "Transfer", "Zach"]
+            .iter()
+            .cloned()
+            .collect();
+    let exclude_positive_categories: HashSet<&'static str> =
+        ["Travel", "Unknown"].iter().cloned().collect();
+
+    if transaction.date < NaiveDate::from_ymd(2020, 3, 14) {
+        return true;
+    }
+    if transaction.date >= NaiveDate::from_ymd(2021, 3, 14) {
+        return true;
+    }
+    if exclude_categories.contains(&*transaction.category) {
+        return true;
+    }
+    if transaction.amount > 0.0 && exclude_positive_categories.contains(&*transaction.category) {
+        return true;
+    }
+
+    false
+}
+
 fn _print_all_transactions() {
     for (source, csv_config, cvs_records) in read_input(PathBuf::from("input")) {
         println!("*** {} ***", source);
@@ -134,123 +253,4 @@ fn _sum_categories() {
     for (category, sum) in &category_sums {
         println!("  {}: {:.2}", category, sum);
     }
-}
-
-#[derive(Debug, Serialize)]
-pub struct TransactionDataNode {
-    name: String,
-    value: u32,
-}
-
-#[derive(Debug, Serialize)]
-pub struct DescriptionDataNode {
-    name: String,
-    children: Vec<TransactionDataNode>,
-}
-
-#[derive(Debug, Serialize)]
-pub struct CategoryDataNode {
-    name: String,
-    children: Vec<DescriptionDataNode>,
-}
-
-#[derive(Debug, Serialize)]
-pub struct RootDataNode {
-    name: String,
-    children: Vec<CategoryDataNode>,
-}
-
-type TransactionDataNodes = Vec<TransactionDataNode>;
-type DescriptionMap = HashMap<String, TransactionDataNodes>;
-type CategoryMap = HashMap<String, DescriptionMap>;
-
-fn generate_icicle_chart_data() {
-    let category_map = gather_category_map();
-    let category_data_nodes = categories_to_data_nodes(category_map);
-    let root_data_node = RootDataNode {
-        name: "Spending".to_string(),
-        children: category_data_nodes,
-    };
-    println!("{}", serde_json::to_string(&root_data_node).unwrap());
-}
-
-fn gather_category_map() -> CategoryMap {
-    let rules = read_rules(PathBuf::from("input"));
-    let classifier = TransactionClassifier::new(rules);
-
-    let mut categories: CategoryMap = HashMap::new();
-
-    for (source, csv_config, cvs_records) in read_input(PathBuf::from("input")) {
-        for csv_record in cvs_records {
-            let transaction = classifier
-                .classify_transaction(csv_record_to_transaction(&csv_record, &csv_config));
-            if !should_exclude_transaction(&transaction) {
-                let descriptions = categories
-                    .entry(transaction.category.clone())
-                    .or_insert(HashMap::new());
-                let transactions = descriptions
-                    .entry(transaction.description.clone())
-                    .or_insert(vec![]);
-                if transaction.amount <= 0.0 {
-                    transactions.push(transaction_to_data_node(&source, transaction));
-                }
-            }
-        }
-    }
-
-    categories
-}
-
-fn transaction_to_data_node(source: &str, transaction: Transaction) -> TransactionDataNode {
-    TransactionDataNode {
-        name: format!("{}, {},", transaction.date, source),
-        value: -transaction.amount.round() as u32,
-    }
-}
-
-fn categories_to_data_nodes(category_map: CategoryMap) -> Vec<CategoryDataNode> {
-    let mut category_data_nodes = vec![];
-    for (category, description_map) in category_map {
-        let mut category_data_node = CategoryDataNode {
-            name: category,
-            children: vec![],
-        };
-        for (description, transaction_data_nodes) in description_map {
-            category_data_node.children.push(DescriptionDataNode {
-                name: description,
-                children: transaction_data_nodes,
-            });
-        }
-        category_data_nodes.push(category_data_node);
-    }
-    category_data_nodes
-}
-
-fn should_exclude_transaction(transaction: &Transaction) -> bool {
-    let exclude_categories: HashSet<&'static str> =
-        ["Dividend", "Salary", "Stock", "Transfer", "Zach"]
-            .iter()
-            .cloned()
-            .collect();
-    let exclude_positive_categories: HashSet<&'static str> =
-        ["Travel", "Unknown"].iter().cloned().collect();
-
-    if transaction.date < NaiveDate::from_ymd(2020, 3, 14) {
-        return true;
-    }
-    if transaction.date >= NaiveDate::from_ymd(2021, 3, 14) {
-        return true;
-    }
-    if exclude_categories.contains(&*transaction.category) {
-        return true;
-    }
-    if transaction.amount > 0.0 && exclude_positive_categories.contains(&*transaction.category) {
-        return true;
-    }
-
-    false
-}
-
-fn main() {
-    generate_icicle_chart_data();
 }
