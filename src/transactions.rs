7c576f8000c53b020d382715eb6477fc231ae30a
Franz Amador <franzamador@gmail.com>
2024-07-10 16:25:17 -0700

    b Allow specifying debit/credit CSV fields

--- src/transactions.rs
+++ src/transactions.rs
@@ -11,10 +11,14 @@ pub struct CsvConfig {
     date_index: usize,
     date_format: String,
     description_index: usize,
+    #[serde(default = "usize::max_value")]
     category_index: usize,
+    #[serde(default = "usize::max_value")]
     amount_index: usize,
-    #[serde(default)]
-    amount_is_debit: bool,
+    #[serde(default = "usize::max_value")]
+    debit_index: usize,
+    #[serde(default = "usize::max_value")]
+    credit_index: usize,
 }
 
 #[derive(Debug)]
@@ -34,36 +38,50 @@ pub fn csv_record_to_transaction(csv_record: &StringRecord, csv_config: &CsvConf
             &csv_config.date_format,
         )
             .unwrap(),
-        raw_description: csv_record
-            .get(csv_config.description_index)
-            .unwrap()
-            .trim()
-            .to_string(),
-        raw_category: csv_record
-            .get(csv_config.category_index)
-            .unwrap()
-            .trim()
-            .to_string(),
-        amount: parse_csv_amount(csv_record, csv_config),
+        raw_description: get_string_field_value(csv_record, csv_config.description_index),
+        raw_category: get_string_field_value(csv_record, csv_config.category_index),
+        amount: get_amount(csv_record, csv_config),
         description: "".to_string(),
         category: "".to_string(),
     }
 }
 
-fn parse_csv_amount(csv_record: &StringRecord, csv_config: &CsvConfig) -> f32 {
-    let amount_str = csv_record
-        .get(csv_config.amount_index)
+fn get_string_field_value(csv_record: &StringRecord, field_index: usize) -> String {
+    if field_index == usize::MAX { return "".to_string(); }
+
+    csv_record
+        .get(field_index)
+        .unwrap()
+        .trim()
+        .to_string()
+}
+
+fn get_amount(csv_record: &StringRecord, csv_config: &CsvConfig) -> f32 {
+    if csv_config.amount_index != usize::MAX {
+        return get_f32_field_value(csv_record, csv_config.amount_index);
+    }
+
+    assert_ne!(csv_config.debit_index, usize::MAX);
+    assert_ne!(csv_config.credit_index, usize::MAX);
+
+    let debit = get_f32_field_value(csv_record, csv_config.debit_index);
+    if debit > 0.0 {
+        return -debit;
+    }
+
+    get_f32_field_value(csv_record, csv_config.credit_index)
+}
+
+fn get_f32_field_value(csv_record: &StringRecord, field_index: usize) -> f32 {
+    let value_str = csv_record
+        .get(field_index)
         .unwrap()
         .replace(',', "");
-    let amount_str = if amount_str.is_empty() { "0.0".to_string() } else { amount_str };
-    let amount: f32 = amount_str
+    if value_str.is_empty() { return 0.0; }
+
+    value_str
         .parse()
-        .unwrap();
-    if csv_config.amount_is_debit {
-        -amount
-    } else {
-        amount
-    }
+        .unwrap()
 }
 
 #[cfg(test)]
@@ -79,7 +97,8 @@ mod tests {
             description_index: 2,
             category_index: 4,
             amount_index: 5,
-            amount_is_debit: false,
+            debit_index: usize::MAX,
+            credit_index: usize::MAX,
         };
         let csv_record = StringRecord::from(vec![
             "ignore1",
